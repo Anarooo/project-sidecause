ce2e32c612dcbfbc37b4e92470973deb676567a5
DESKTOP-J414AWE231 <alexmeade12345@outlook.com>
2020-12-10 21:06:38 +0000

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const sidecause = new Discord.Client();
-const TOKEN = "Nzg2NjU1Mjc3MDMzMzI0NTQ0.X9Jj0Q.PXYY0T-pNbLpfS5qUA6zAPst9C8"
+const TOKEN = process.env.BOT_TOKEN
 
 sidecause.once('ready', () => {
     console.log('Ready!');
