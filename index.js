10d5bfa23f12577f7ae1d52a93db218cc3089d00
DESKTOP-J414AWE231 <alexmeade12345@outlook.com>
2020-12-17 23:17:03 +0000

    v1.1.2-a (Command Library commit)

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const { prefix, secret_code } = require('./config.json');
 
 const sidecause = new Discord.Client();
 sidecause.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const TOKEN = process.env.TOKEN
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    sidecause.commands.set(command.name, command);
+};
+
 
 sidecause.once('ready', () => {
     console.log('Bot is now online.');
