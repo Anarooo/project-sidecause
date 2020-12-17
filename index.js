8d8434f00188917ebb92e3242c7b307c1edce847
DESKTOP-J414AWE231 <alexmeade12345@outlook.com>
2020-12-17 23:14:45 +0000

    v0.1.2-a Commit (Testing Libraries.

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 require('dotenv').config();
 console.log(process.env.TOKEN)
+const fs = require('fs');
 const Discord = require('discord.js');
-const sidecause = new Discord.Client();
-const TOKEN = process.env.TOKEN
 const { prefix, secret_code } = require('./config.json');
 
+const sidecause = new Discord.Client();
+sidecause.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const TOKEN = process.env.TOKEN
 
 sidecause.once('ready', () => {
-    console.log('Ready!');
+    console.log('Bot is now online.');
 });
 
 sidecause.on('message', message => {
     console.log(message.content);
-    if (message.content === `${prefix}ping`){
-        message.channel.send('Pong.');
-    }
-    else if (message.content === `${secret_code}`){
-        message.channel.send('Secret Code Works')
-    }
 });
 
 sidecause.login(TOKEN)
