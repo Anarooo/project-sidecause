61b3fdd8b6dd0a474da2b02f0db203b17ec3a359
DESKTOP-J414AWE231 <alexmeade12345@outlook.com>
2020-12-17 23:26:05 +0000

    v0.1.1-a Debug

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, secret_code } = require('./config.json');
 
+const TOKEN = process.env.TOKEN
 const sidecause = new Discord.Client();
 sidecause.commands = new Discord.Collection();
-const TOKEN = process.env.TOKEN
+
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
     console.log(`${file}`)
