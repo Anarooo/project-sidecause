a2a73112f251f56fcab718626b29cd088339c839
DESKTOP-J414AWE231 <alexmeade12345@outlook.com>
2020-12-18 14:24:11 +0000

    v0.1.1-a Kick Command

---
+++ commands/kick.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'kick',
+    description: 'Kick a user',
+    execute(message, args) {
+        if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You do not have the permission to use this command")
+        let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+        if (!User) return message.channel.send("You have not provided a valid user to kick.")
+        if (User.hasPermission("KICK_MEMBERS")) return message.reply("You do not have the permission to use this command")
+        let kickReason = args.join(" ").slice(22);
+        if (!kickReason) {
+            kickReason = "No Reason Provided"
+        }
+        let {guild} = message
+        message.User.send(`You were kicked from ${guild.name} for ${kickReason}`)
+        User.kick({reason: kickReason})
+    },
+};
