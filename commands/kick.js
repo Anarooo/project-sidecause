dcee12f3e31a0b412778eba606467527f8615661
DESKTOP-J414AWE231 <alexmeade12345@outlook.com>
2020-12-18 14:43:34 +0000

    v0.1.1-a Kick Debug

--- commands/kick.js
+++ commands/kick.js
@@ -2,17 +2,16 @@ module.exports = {
     name: 'kick',
     description: 'Kick a user',
     execute(message, args) {
-        if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You do not have the permission to use this command")
-        const kickedUser = message.mentions.members.first();
-        let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-        if (!User) return message.channel.send("You have not provided a valid user to kick.")
-        if (User.hasPermission("KICK_MEMBERS")) return message.reply("You do not have the permission to use this command")
-        let kickReason = args.join(" ").slice(22);
-        if (!kickReason) {
-            kickReason = "No Reason Provided"
+        if (message.member.hasPermission("KICK_MEMBERS")) {
+            const kickedUser = message.members.mentions.first()
+            if (kickedUser) {
+                try {
+                    kickReason = args.join(" ").slice(22);
+                    let {guild} = message
+                    kickedUser.user.send(`You were kicked from ${guild.name} for ${kickReason}`)
+                    kickedUser.kick({reason: kickReason})
+                }
+            }
         }
-        let {guild} = message
-        kickedUser.send(`You were kicked from ${guild.name} for ${kickReason}`)
-        kickedUser.kick({reason: kickReason})
     },
 };
