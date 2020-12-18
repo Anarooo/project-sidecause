8d4a64a57e2a5f59f87b83b44d36c67430255421
DESKTOP-J414AWE231 <alexmeade12345@outlook.com>
2020-12-18 15:25:37 +0000

    ban command

---
+++ commands/ban.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: 'ban',
+    description: 'Bans a user',
+    execute(message, args) {
+        if (message.member.hasPermission("BAN_USERS")) {
+            const bannedUser = message.mentions.members.first()
+            const banOperator = message.member()
+            if (message.mentions.users.first()) {
+                try {
+                    let {guild} = message
+                    message.mentions.users.first().send(`You were banned from ${guild.name} by ${banOperator.name}.`)
+                    setTimeout(() => {
+                        bannedUser.ban(`User was banned by ${banOperator.name}`);
+                    }, 2000);
+                    
+                } catch {
+                    message.reply(`I do not have permission to ban ${bannedUser}`)}
+                }else {
+                    message.reply(`You do not have permissions to ban ${bannedUser}`)
+            }
+        }
+    }
+};
